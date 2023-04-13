650d0ac92a398c9ed80d40ea05752e895874b64d
krela <krel.andrej@yandex.ru>
2023-04-13 14:27:40 +0700

    1 com

---
+++ src/main/java/com/example/homeworck28/Homeworck28Application.java
@@ -0,0 +1,13 @@
+package com.example.homeworck28;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homeworck28Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homeworck28Application.class, args);
+    }
+
+}
